9645e9c4720aa115532260f3da8deb5d2b1f6b0a
Paulo H. Lopes <phlopes93@gmail.com>
2021-02-25 00:32:20 -0300

    programa maior_2_numeros

---
+++ bloco-4-introducao-a-js-e-logica/dia-1/maior_2_numeros.js
@@ -0,0 +1,13 @@
+let n1 = 11;
+let n2 = 13;
+
+if (n1 > n2){
+    console.log('O Maior número é '+ n1);
+} else if (n2>n1){
+    console.log('O Maior número é '+ n2); 
+} else {
+    console.log ('Você digitou números iguais, favor digitar 2 números diferentes.');
+}
+
+
+
