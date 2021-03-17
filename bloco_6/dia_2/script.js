4bba31b46163c783fa20c1c43b49ea8df890cf16
Paulo H. Lopes <phlopes93@gmail.com>
2021-03-17 09:53:17 -0300

    estilizaçao form

---
+++ bloco_6/dia_2/script.js
@@ -0,0 +1,48 @@
+let estadosBrasil = {
+    '': 'Selecione seu estado',
+    'AC': 'Acre',
+    'AL': 'Alagoas',
+    'AP': 'Amapá',
+    'AM': 'Amazonas',
+    'BA': 'Bahia',
+    'CE': 'Ceará',
+    'DF': 'Distrito Federal',
+    'ES': 'Espírito Santo',
+    'GO': 'Goiás',
+    'MA': 'Maranhão',
+    'MT': 'Mato Grosso',
+    'MS': 'Mato Grosso do Sul',
+    'MG': 'Minas Gerais',
+    'PA': 'Pará',
+    'PB': 'Paraíba',
+    'PR': 'Paraná',
+    'PE': 'Pernambuco',
+    'PI': 'Piauí',
+    'RJ': 'Rio de Janeiro',
+    'RN': 'Rio Grande do Norte',
+    'RS': 'Rio Grande do Sul',
+    'RO': 'Rondônia',
+    'RR': 'Roraima',
+    'SC': 'Santa Catarina',
+    'SP': 'São Paulo',
+    'SE': 'Sergipe',
+    'TO': 'Tocantins'
+};
+let estadosBr = document.getElementById('estadosbr');
+
+for (let key in estadosBrasil) {
+    let createOption = document.createElement('option');
+    createOption.innerText = estadosBrasil[key];
+    createOption.value = [key];
+    estadosBr.appendChild(createOption);
+};
+
+let clear = document.querySelector('.button-clear');
+    clear.addEventListener('click', () => {
+    let inputs = document.querySelectorAll('input');
+    let textArea = document.querySelector('textarea');
+    for (let i = 0; i < inputs.length; i += 1) {
+        inputs[i].value = ''
+        textArea.value = ''
+    }
+    });
